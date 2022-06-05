7f2216262f7716b6ba430d6d4ff691391a6f909a
Filterbot12 <105578959+Filterbot12@users.noreply.github.com>
2022-06-05 21:21:13 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -42,7 +42,7 @@ async def start(client, message):
             InlineKeyboardButton('➕ 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🔊 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 🔊', url='https://t.me/msmOTT'),
-            InlineKeyboardButton('💬 𝚄𝙿𝙳𝙰𝚃𝙴𝚂 💬', url='https://t.me/MSMchat')
+            InlineKeyboardButton('💬 𝙶𝚁𝙾𝚄𝙿 💬', url='https://t.me/MSMchat')
             ],[      
             InlineKeyboardButton('ℹ️ 𝙷𝙴𝙻𝙿 ℹ️', callback_data='help'),
             InlineKeyboardButton('♻️ 𝙰𝙱𝙾𝚄𝚃 ♻️', callback_data='about')
@@ -87,7 +87,7 @@ async def start(client, message):
 
             InlineKeyboardButton('🔊 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 🔊', url='https://t.me/msmOTT'),
 
-            InlineKeyboardButton('💬 𝚄𝙿𝙳𝙰𝚃𝙴𝚂 💬', url='https://t.me/MSMchat')
+            InlineKeyboardButton('💬 𝙶𝚁𝙾𝚄𝙿 💬', url='https://t.me/MSMchat')
 
             ],[      
 
