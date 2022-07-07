00e7d7cde97e51527d04bbbd720b013ee58b7cfe
ααdhí | αrjun vr <87893573+Aadhi000@users.noreply.github.com>
2022-07-07 11:12:21 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -239,6 +239,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('Test Button', url='youtube.com/OpusTechz') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
